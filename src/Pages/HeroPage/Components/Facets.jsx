36acd79183fff6b58d6c2b6287bd094a970cc986
AungPhoneMaw <khant09.kz@gmail.com>
2026-02-13 23:11:20 +0100

    add facet icon

--- src/Pages/HeroPage/Components/Facets.jsx
+++ src/Pages/HeroPage/Components/Facets.jsx
@@ -2,9 +2,16 @@ import {use, useEffect, useState } from "react";
 import "../ComponentsStyle/Facets.css"
 
 
-function Facet({facetName, facetColor}){
+function Facet({facetName, facetColor, facetIcon}){
     return(
-        <div className="facet-container" style={{backgroundColor : facetColor}}>{facetName}</div>
+        
+        <div className="facet-container" style={{backgroundColor : `rgb(from ${facetColor} r g b / 0.3)`}}>
+            <div className="facet-icon-container">
+                <img className="facet-icon"
+                src={`https://cdn.steamstatic.com/apps/dota2/images/dota_react/icons/facets/${facetIcon}.png`}/>
+            </div>
+            <h1 className="facet-title">{facetName}</h1>
+        </div>
     )
 }
 
@@ -17,7 +24,8 @@ export function Facets({heroName, facets}){
         <div className="facets-container">
             {facets.map((facet)=>{
                 return(
-                    <Facet facetName={facet["title"]} facetColor = {facet["color"]} />
+                    <Facet facetName={facet["title"]} facetColor = {facet["color"]} 
+                    facetIcon= { facet["icon"] }/>
                 )
             })}
         </div>
