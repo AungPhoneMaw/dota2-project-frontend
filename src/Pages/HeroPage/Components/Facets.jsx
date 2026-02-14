67211122847a79e3cc60433c46fa3f46921e5076
AungPhoneMaw <khant09.kz@gmail.com>
2026-02-14 16:51:33 +0100

    fix MaainContents overflowing

--- src/Pages/HeroPage/Components/Facets.jsx
+++ src/Pages/HeroPage/Components/Facets.jsx
@@ -5,7 +5,7 @@ import "../ComponentsStyle/Facets.css"
 function Facet({facetName, facetColor, facetIcon}){
     return(
         
-        <div className="facet-container" style={{backgroundColor : `rgb(from ${facetColor} r g b / 0.3)`}}>
+        <div className="facet-container" style={{backgroundColor : `rgb(from ${facetColor} r g b / 0.25)`}}>
             <div className="facet-icon-container">
                 <img className="facet-icon"
                 src={`https://cdn.steamstatic.com/apps/dota2/images/dota_react/icons/facets/${facetIcon}.png`}/>
